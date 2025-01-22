90b4f00d3fa42219ea06794b1841548b65fcd6cd
Xue Xuan Tan <s10267817@connect.np.edu.sg>
2025-01-22 15:34:44 +0800

    load airliness

--- assignment/Program.cs
+++ assignment/Program.cs
@@ -1,4 +1,48 @@
 ﻿using assignment;
+
+Dictionary<string, Airline> airlineDict= new Dictionary<string, Airline>();
+//load airline.csv
+using (StreamReader sr = new StreamReader("airlines.csv"))
+{
+    string? s = sr.ReadLine(); // read the heading
+                               // display the heading
+    while ((s = sr.ReadLine()) != null)
+    {
+        string[] airinfo = s.Split(',');
+        string airlineName = airinfo[0].Trim();
+        string airlineCode = airinfo[1].Trim();
+        Airline airline = new Airline(airlineName, airlineCode);
+        airlineDict[airlineCode] = airline;
+    }
+}
+//display airline dict
+foreach (var airline in airlineDict.Values)
+{
+    Console.WriteLine(airline);
+}
+
+Dictionary<string, BoardingGate> boardingGateDict = new Dictionary<string, BoardingGate>();
+using(StreamReader sr = new StreamReader("boardinggates.csv"))
+{
+    string?s =sr.ReadLine();
+    while ((s=sr.ReadLine()) != null)
+    {
+        string[] boardgateinfo = s.Split(',');
+        string gate= boardgateinfo[0].Trim();
+        bool ddjb = bool.Parse(boardgateinfo[1].Trim());
+        bool cfft = bool.Parse(boardgateinfo[2].Trim());
+        bool lwtt = bool.Parse(boardgateinfo[3].Trim());
+        Flight flight = null;
+        BoardingGate boardingGate = new BoardingGate(gate, ddjb, cfft, lwtt, flight);
+        boardingGateDict[gate] = boardingGate;
+    }
+
+}
+foreach (var boardingGate in boardingGateDict.Values)
+{
+    Console.WriteLine(boardingGate);
+}
+
 void MainMenu()
 {
     Console.WriteLine("=============================================");
@@ -34,6 +78,7 @@ using (StreamReader sr = new StreamReader("Flight.csv"))
             specialRequestCode = parts[4].Trim();
         }
         Flight flight;
+
 
         if (specialRequestCode == "DDJB")
         {
